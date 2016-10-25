da512a4ae181d0476c14bdf77fda8c5c19f7130b
n1crack <yusuf@ozdemir.be>
2016-10-25 23:53:48 +0300

    add .travis.yml for testing

--- tests/spec/DatatablesSpec.php
+++ tests/spec/DatatablesSpec.php
@@ -10,14 +10,8 @@ class DatatablesSpec extends ObjectBehavior {
 
     function let()
     {
-        /// SQLite Testing
-        $app_path = realpath(dirname(__FILE__) . '/../../');
-        $sqlconfig = $app_path . '/db/sqlite-sakila.sqlite';
-        /// Mysql Testing
-        $mysqlconfig = ['host' => 'localhost', 'port' => '3306', 'username' => 'homestead', 'password' => 'secret', 'database' => 'sakila'];
-
+        $sqlconfig = realpath(dirname(__FILE__) . '/test.db');
         $db = new SQLite($sqlconfig);
-        //$db = new MySQL($mysqlconfig);
 
         $this->beConstructedWith($db);
     }
@@ -34,21 +28,21 @@ class DatatablesSpec extends ObjectBehavior {
 
     public function it_returns_record_counts()
     {
-        $this->query("Select film_id as fid, title, description from film where film_id > 655");
+        $this->query("Select id as fid, name, surname, age from mytable where id > 3");
         $datatables = $this->generate(false);
-        $datatables['recordsTotal']->shouldReturn(345);
-        $datatables['recordsFiltered']->shouldReturn(345);
+        $datatables['recordsTotal']->shouldReturn(8);
+        $datatables['recordsFiltered']->shouldReturn(8);
     }
 
     public function it_returns_data_from_a_basic_sql()
     {
-        $this->query("Select film_id as fid, title, description from film");
+        $this->query("Select id as fid, name, surname, age from mytable");
 
         $data = $this->generate(false)['data'][0];
 
         $data['fid']->shouldReturn("1");
-        $data['title']->shouldReturn("ACADEMY DINOSAUR");
-        $data['description']->shouldContain('A Epic Drama of a');
+        $data['name']->shouldReturn("John");
+        $data['surname']->shouldContain('Doe');
     }
 
     public function it_sets_column_names_from_aliases()
@@ -66,47 +60,27 @@ class DatatablesSpec extends ObjectBehavior {
 
     public function it_returns_modified_data_via_closure_function()
     {
-        $this->query("Select film_id as fid, title, description from film");
+        $this->query("Select id as fid, name, surname, age from mytable");
 
-        $this->edit('title', function ($data)
+        $this->edit('name', function ($data)
         {
-            return strtolower($data['title']);
+            return strtolower($data['name']);
         });
 
-        $this->edit('description', function ($data)
+        $this->edit('surname', function ($data)
         {
-            return $this->customfunction($data['description']);
+            return $this->customfunction($data['surname']);
         });
 
-        $data = $this->generate(false)['data']['0'];
+        $data = $this->generate(false)['data']['2'];
 
-        $data['title']->shouldReturn('academy dinosaur');
-        $data['description']->shouldReturn('A Epic Dra...');
+        $data['name']->shouldReturn('george');
+        $data['surname']->shouldReturn('Mar...');
     }
 
     function customfunction($data)
     {
-        return substr($data, 0, 10) . '...';
-    }
-
-    public function it_returns_data_from_a_more_complex_sql()
-    {
-        $this->query("Select 
-          (category.name) as category_name, 
-          sum(length) as total_length 
-        from film 
-        left join film_category on film_category.film_id = film.film_id
-        left join category on film_category.category_id = category.category_id
-        group by category.category_id");
-
-        $this->edit('total_length', function ($data){
-            return $data['total_length'] . ' minutes';
-        });
-        $data = $this->generate(false)['data'][4];
-
-        $data['category_name']->shouldReturn("Comedy");
-        $data['total_length']->shouldReturn("6718 minutes");
-        $data->shouldHaveColumns(['category_name', 'total_length']);
+        return substr($data, 0, 3) . '...';
     }
 
     public function it_returns_column_names_from_query_that_includes_a_subquery_in_select_statement()
