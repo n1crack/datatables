404efb5417e4a916f4ea9e5503dc27bfda0cf156
KaidoND <kaido@nordic-digital.ee>
2018-07-25 12:46:01 +0300

    Added prestashop adapter

---
+++ src/DB/PSAdapter.php
@@ -0,0 +1,38 @@
+<?php namespace Ozdemir\Datatables\DB;
+
+use Db;
+
+class PSAdapter implements DatabaseInterface {
+
+    protected $Db;
+    protected $config;
+    protected $escape = [];
+
+    function __construct($config)
+    {
+        $this->config = $config;
+    }
+
+    public function connect()
+    {
+        $this->Db = Db::getInstance();
+
+        return $this->Db;
+    }
+
+    public function query($query, $array = true, $user_cache = true)
+    {
+        return $this->Db->executeS($query);
+    }
+
+    public function count($query)
+    {
+        return $this->Db->getValue($query);
+    }
+
+    public function escape($value)
+    {
+        return pSQL($value);
+    }
+
+}
