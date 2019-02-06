b1892de66921b378da50948ac263383499ffb511
n1crack <yusuf@ozdemir.be>
2019-02-06 22:21:47 +0300

    add PhpDoc Blocks

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -2,29 +2,56 @@
 
 namespace Ozdemir\Datatables\Http;
 
+/**
+ * Class Request
+ * @package Ozdemir\Datatables\Http
+ */
 class Request
 {
 
+    /**
+     * @var ParameterBag
+     */
     public $request;
 
+    /**
+     * @var ParameterBag
+     */
     public $query;
 
+    /**
+     * Request constructor.
+     * @param array $request
+     * @param array $query
+     */
     public function __construct($request = array(), $query = array())
     {
         $this->request = new ParameterBag($request);
         $this->query = new ParameterBag($query);
     }
 
+    /**
+     * @return Request
+     */
     public static function createFromGlobals(): Request
     {
         return self::create($_POST, $_GET);
     }
 
+    /**
+     * @param array $request
+     * @param array $query
+     * @return Request
+     */
     public static function create($request = array(), $query = array()): Request
     {
         return new self($request, $query);
     }
 
+    /**
+     * @param $key
+     * @return mixed|null
+     */
     public function get($key)
     {
         if ($this->request->has($key)) {
